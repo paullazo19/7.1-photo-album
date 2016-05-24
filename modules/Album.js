a729a6099d84ccd1588a058df395afb15ee668eb
Paul Lazo <paullazo19@gmail.com>
2016-05-24 07:23:49 -0500

    added partial modal functionality to photos app

--- modules/Album.js
+++ modules/Album.js
@@ -2,31 +2,40 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default React.createClass({
-  getInitial(){
+  getInitialState(){
     return {
-        isOpen: false
+        isOpen: false,
+        currentPhoto: this.props.album.photos[0]
     }
   },
   handlePhotoClick(e){
-    console.log(e.target);
     this.setState(
       {
-        isOpen: true
+        isOpen: true,
+        currentPhoto: this.props.album.photos[e.currentTarget.id]
+      }
+    )
+  },
+  closeModal(e){
+    this.setState(
+      {
+        isOpen: false
       }
     )
   },
   render() {
+    console.log(this.state.currentPhoto.src);
     return (
-    <section>
-      <ul className="album__thumbnail--list">
+    <section className="album__thumbnail--list">
+      <ul>
         <h3 className="album__thumbnail--heading">{this.props.album.title}</h3>
         {this.props.album.photos.map(function(photo, i){
-          return <li key={i} className="album__thumbnail" onClick={this.handlePhotoClick}><div className="album__thumbnail--image"><img key={i} src={`../assets/${photo.src}`} alt={photo.title} /></div><h5 className="album__thumbnail--title">{photo.title}</h5></li>
+          return <li key={i} id={i} className="album__thumbnail" onClick={this.handlePhotoClick}><div className="album__thumbnail--image"><img key={i} src={`../assets/${photo.src}`} alt={photo.title} /></div><h5 className="album__thumbnail--title">{photo.title}</h5></li>
 
         }, this)
         }
       </ul>
-      <div className="hidden" isOpen={this.props.isOpen ? "modal" : "hidden"}></div>
+      <div className={this.state.isOpen ? "modal" : "hidden"} photo={this.state.currentPhoto}><button className="modal__backButton" onClick={this.closeModal}>Back to {this.props.album.title}</button><h2 className="album__photo--title">{this.state.currentPhoto.title}</h2><img className="album__photo--fullSize" src={`../assets/${this.state.currentPhoto.src}`}/></div>
 
     </section>
     )
