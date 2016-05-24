af02456beec0109fa272afcfbeb44c586660a626
Paul Lazo <paullazo19@gmail.com>
2016-05-24 16:24:43 -0500

    added forward and back arrows on modal screen

--- modules/Album.js
+++ modules/Album.js
@@ -12,25 +12,38 @@ export default React.createClass({
     this.setState(
       {
         isOpen: true,
-        currentPhoto: this.props.album.photos[e.currentTarget.id]
+        currentPhoto: this.props.album.photos[e.currentTarget.id],
+        currentPhotoID: e.currentTarget.id
       }
     )
   },
   leftArrowClick(e){
-    console.log(this.props.album.photos[(e.currentTarget.id)--]);
-    this.setState(
-      {
-        currentPhoto: this.props.album.photos[(e.currentTarget.id)--]
-      }
-    )
+    var newCurrentPhotoID = (Number(this.refs.modalImage.id) - 1);
+    if (newCurrentPhotoID < 0) {
+      //disable button
+    }else{
+      this.setState(
+        {
+          currentPhoto: this.props.album.photos[newCurrentPhotoID],
+          currentPhotoID: newCurrentPhotoID
+        }
+      )
+    }
+
   },
   rightArrowClick(e){
-    console.log(this.props.album.photos[(e.currentTarget.id)++]);
-    this.setState(
-      {
-        currentPhoto: this.props.album.photos[(e.currentTarget.id)++]
-      }
-    )
+    var newCurrentPhotoID = (Number(this.refs.modalImage.id) + 1);
+    if(newCurrentPhotoID >= this.props.album.photos.length){
+
+    }else{
+      this.setState(
+        {
+          currentPhoto: this.props.album.photos[newCurrentPhotoID],
+          currentPhotoID: newCurrentPhotoID
+        }
+      )
+    }
+
   },
   closeModal(e){
     this.setState(
@@ -45,12 +58,16 @@ export default React.createClass({
       <ul>
         <h3 className="album__thumbnail--heading">{this.props.album.title}</h3>
         {this.props.album.photos.map(function(photo, i){
-          return <li key={i} id={i} className="album__thumbnail" onClick={this.handlePhotoClick}><div className="album__thumbnail--image"><img key={i} src={`../assets/${photo.src}`} alt={photo.title} /></div><h5 className="album__thumbnail--title">{photo.title}</h5></li>
+          return <li key={i} id={i} className="album__thumbnail" onClick={this.handlePhotoClick}>
+          <div className="album__thumbnail--image">
+            <img key={i} src={`../assets/${photo.src}`} alt={photo.title}/>
+          </div><h5 className="album__thumbnail--title">{photo.title}</h5></li>
 
         }, this)
         }
       </ul>
-      <div className={this.state.isOpen ? "modal" : "hidden"} photo={this.state.currentPhoto}><button className="modal__backButton" onClick={this.closeModal}><i className="fa fa-chevron-left icon--back"></i>Back to {this.props.album.title}</button><h2 className="album__photo--title">{this.state.currentPhoto.title}</h2><i className="fa fa-arrow-circle-left fa-3x icon--left" onClick={this.leftArrowClick}></i><img className="album__photo--fullSize" src={`../assets/${this.state.currentPhoto.src}`}/><i className="fa fa-arrow-circle-right fa-3x icon--right" onClick={this.rightArrowClick}></i></div>
+      <div className={this.state.isOpen ? "modal" : "hidden"} photo={this.state.currentPhoto}><button className="modal__backButton" onClick={this.closeModal}><i className="fa fa-chevron-left icon--back"></i>Back to {this.props.album.title}</button><h2 className="album__photo--title">{this.state.currentPhoto.title}</h2><i className="fa fa-arrow-circle-left fa-3x icon--left" onClick={this.leftArrowClick}></i>
+      <img className="album__photo--fullSize" ref="modalImage" id={this.state.currentPhotoID} src={`../assets/${this.state.currentPhoto.src}`}/><i className="fa fa-arrow-circle-right fa-3x icon--right" onClick={this.rightArrowClick}></i></div>
 
     </section>
     )
